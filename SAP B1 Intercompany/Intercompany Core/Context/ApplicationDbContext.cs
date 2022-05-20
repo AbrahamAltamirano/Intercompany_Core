09931f2330d3ccb5b3dd68ac6aba1dee53dffede
AbrahamAltamirano <79951601+AbrahamAltamirano@users.noreply.github.com>
2022-05-20 16:36:54 -0500

    Add files via upload

---
+++ SAP B1 Intercompany/Intercompany Core/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Intercompany_Core;
+using Intercompany_Core.Entities;
+
+namespace Intercompany_Core
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Transaccion> Transaccion { get; set; }
+    }
+}
---
+++ SAP B1 Intercompany/Intercompany Core/Entities/Transaccion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Intercompany_Core.Entities
+{
+    public class Transaccion
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime FechaTransaccion { get; set; }
+        public DateTime FechaSincronizacion { get; set; }
+        public int TipoTransaccion { get; set; }
+        public string TipodCRUD { get; set; }
+        public int IdOrigen { get; set; }
+        public int IdDestino { get; set; }
+        public char Sincronizado { get; set; }
+        public string ErrorDesc { get; set; }
+        public string JSON { get; set; }
+        public int IdObjeto { get; set; }
+    }
+}
